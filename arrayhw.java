c14a030247217e7f8edac93003ce309981a237ff
kaganyazici2 <159692959+kaganyazici2@users.noreply.github.com>
2024-02-11 16:32:53 +0300

    Update arrayhw.java
    added some comments

--- arrayhw.java
+++ arrayhw.java
@@ -15,33 +15,34 @@ public class arrayhw {
         class2 test2 = new class2();
         int choice = 0;
 
-        //creating the array
+        // creating the array
         for (int i = 0; i < array.length; i++){
             array[i] = rd.nextInt(101);
         }
         System.out.println("the array is: " + Arrays.toString(array));
         System.out.println();
 
+        // displaying the menu options until user enters 4
         do {
             test1.displayMenu();
             System.out.print("enter the option: ");
             choice = sc.nextInt();
             System.out.println();
-
+        // print maximum and minimum of the array
             if (choice == 1){
                 System.out.println("min. of the array: " + test1.findMinOfArray(array));
                 System.out.println("max. of the array: " + test1.findMaxOfArray(array));
             }
-            
+        // print the average and deviation of each element from the average
             else if (choice == 2){
                 System.out.println(Arrays.toString(test2.findAverageAndDeviations(array)));
             }
-            
+        // print the sum of even and odd indexes
             else if (choice == 3){
                 System.out.println("sum of odd indexes: " + test2.sumOfIndexes(array, true));
                 System.out.println("sum of even indexes: " + test2.sumOfIndexes(array, false));
             }
-
+        // exit
             else if (choice == 4){
                 System.out.println("Goodbye...");
             }
